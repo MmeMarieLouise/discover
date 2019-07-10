2b638cdfd981b5cd7de32e825af44af141758933
Marie-Louise Annan <ml_annan@hotmail.com>
2019-07-10 23:54:30 +0100

    add author & publisher to results, add comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,8 +7,15 @@ function bookSearch(){
         dataType: 'json',
         success: function(data){
             for(i = 0; i < data.items.length; i++) {
+                // title
                 results.innerHTML += "<h3>" + data.items[i].volumeInfo.title + "<h3>"
+                // description
                 results.innerHTML += "<p>" + data.items[i].volumeInfo.description + "<p>"
+                // author
+                results.innerHTML += "<p>" + data.items[i].volumeInfo.authors + "<p>"
+                // publisher
+                results.innerHTML += "<p>" + data.items[i].volumeInfo.publisher + "<p>"
+
             }
         },
         type:'GET'
