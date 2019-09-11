77cff037de5adef9647475589e70ef9bd110c075
Marie-Louise Annan <m.annan@wellcome.ac.uk>
2019-09-11 13:52:01 +0100

    add .map method on innerHTML

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,71 +18,75 @@ function asyncReq (searchQuery) {
 }
 
 function successResult (data) {
-  for (i = 0; i < data.items.length; i++) {
 
-    const volume = data.items[i].volumeInfo;
-    const title = volume.title;
-    const author = volume.authors;
-    const cover = volume.imageLinks.smallThumbnail;
-    const synopsis = volume.description;
-    const publisher = volume.publisher;
-    const preview = volume.previewLink;
-    printTitle(title);
-    printAuthor(author);
-    displayCover(cover);
-    printSynopsis(synopsis);
-    printPublisher(publisher);
-    linkPreview(preview);
-  }
+  /* .map method returns a new array,  markUp loops over each book and creates a new array item, the first param must be a function and its param is the array item `book` that will be looped over. Then, .join converts an array into a string (empty string passed through .join allows empty space to be printed).Then call 'printTitle' function to print string. Finally add .innerHTML so that it adds the mark up to the page once.
+  Template literals `${string}` are used to allow embedded expressions.
+   */
+
+
+    const markUp = data.items.map(function(book){
+      return ` 
+        <div>
+          ${printTitle(book.volumeInfo.title)}
+          ${printAuthor(book.volumeInfo.authors)}
+          ${displayCover(book.volumeInfo.imageLinks.smallThumbnail)}
+          ${printSynopsis(book.volumeInfo.description)}
+          ${printPublisher(book.volumeInfo.publisher)}
+          ${linkPreview(book.volumeInfo.previewLink)}
+        </div>
+      `
+    }).join('');
+
+    results.innerHTML = markUp;
 }
 
 function printTitle(title) {
   if (title) {
-    results.innerHTML += "<h3>" + title + "</h3>"
+   return "<h3>" + title + "</h3>"
   }
   else {
-    results.innerHTML += "<p>Title: Sorry, this content is not available. Please check out the preview link below.</p>"
+    return "<p>Title: Sorry, this content is not available. Please check out the preview link below.</p>"
   }
 }
 
-function printAuthor(author) {
-   if (author) {
-  results.innerHTML += "<p>Author(s): " + author + "</p>"
+function printAuthor(authors) {
+   if (authors) {
+  return "<p>Author(s): " + authors + "</p>"
    }
   else {
-  results.innerHTML += "<p>Author(s): Sorry, this content is not available. Please check out the preview link below.</p>"
+  return "<p>Author(s): Sorry, this content is not available. Please check out the preview link below.</p>"
   }
 }
 
-function displayCover(cover) {
-   if (cover) {
-  $("#results").append("<a href=\" \"><img id=\"each\" class=\"thumbnail\" src=" + cover + "></a>")
+function displayCover(imageLinks) {
+   if (imageLinks) {
+  return `<img id="each" class="thumbnail" src="${imageLinks} "></a>`;
    } else {
-     results.innerHTML += "<p>Cover Image: Sorry, this content is not available. Please check out the preview link below.</p>"
+     return "<p>Cover Image: Sorry, this content is not available. Please check out the preview link below.</p>"
    }
 }
 
-function printSynopsis(synopsis){
-  if (synopsis) {
-   results.innerHTML += "<p>Synopsis: " + synopsis + "</p>"
+function printSynopsis(description){
+  if (description) {
+   return "<p>Synopsis: " + description + "</p>"
   } else {
-  results.innerHTML += "<p>Synopsis: Sorry, this content is not available. Please check out the preview link below.</p>"
+  return"<p>Synopsis: Sorry, this content is not available. Please check out the preview link below.</p>"
  }
 }
 
 function printPublisher(publisher){
   if (publisher) {
- results.innerHTML += "<p>Publisher: " + publisher + "</p>"
+ return "<p>Publisher: " + publisher + "</p>"
   } else {
-  results.innerHTML += "<p>Publisher: Sorry, this content is not available. Please check out the preview link below.</p>"
+  return "<p>Publisher: Sorry, this content is not available. Please check out the preview link below.</p>"
    }
 }
 
-function linkPreview(preview){
-  if (preview) {
-  results.innerHTML += "<a target=\"_blank\" rel=\"noopener noreferrer\" href =" + preview + "> Check out a preview <a/>"
+function linkPreview(previewLink){
+  if (previewLink) {
+  return "<a target=\"_blank\" rel=\"noopener noreferrer\" href =" + previewLink + "> Check out a preview <a/>"
   } else {
-  results.innerHTML += "<p>Preview Link: Sorry, this content is not available. Please try again later.</p>"
+  return "<p>Preview Link: Sorry, this content is not available. Please try again later.</p>"
   }
 }
 
