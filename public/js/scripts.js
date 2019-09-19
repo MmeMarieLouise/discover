7f42610a330045100f412ba95f879b65c0825149
Marie-Louise Annan <ml_annan@hotmail.com>
2019-09-19 10:55:53 +0100

    finish comments & refactoring

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,35 +4,37 @@
 /*********************************************************************************************************************/
 
 const bookSearch = () => {
-    // the users input is stored in var `find`
+    // the users input is stored in const `find`
     const find = document.getElementById('textName').value;
     // replace the value of div `results` and set it to empty string
     document.getElementById('results').innerHTML = "";
-    // promise
-    asyncReq(find).then((data) => successResult(data))
+    // to get value out from promise object, call Promise API methods (.then, .catch) on asynReq to consume the value of the fullfilled promise - .then()
+    asyncReq(find).then((data) => successResult(data)).catch(error => console.log(error))
 }
 
-// AJAX request function, param stores search query results  - AJAX or Asynchronous JavaScript And XML is the use of the XMLHttpRequest object to communicate with servers without reloading the page. the ajax request itself has 4 server responses /status properties `readyState`, `onreadystatechange`, `status`, `statusText`
+// AJAX request function, param stores search query results  - AJAX or Asynchronous JavaScript And XML is the use of the XMLHttpRequest object to communicate with servers without reloading the page. The AJAX request itself has 4 server responses /status properties `readyState`, `onreadystatechange`, `status`, `statusText`
 const asyncReq = (searchQuery) => {
-  // promise is returned on `asyncReq` - promise(((executor functions)
+  // create promise instance using `Promise()` constructor. - promise is returned on `asyncReq` - Promise(((executor functions)
     return new Promise (((resolve,reject) => {
-      // XMLHttpRequest object is created in order to make used to communicate with the server
+      // XMLHttpRequest object is created
             const xhr = new XMLHttpRequest();
-            // .onreadystatechange property (contains an event handler `readystatechange`) defines a function to be called when the readyState property changes
+            // callback runs each time there is a change of the ajax request - .onreadystatechange property (contains an event handler `readystatechange`) defines a function to be called when the readyState property changes
         xhr.onreadystatechange = (() => {
           // readyState holds status of XMLHttpRequest object, 4 = request finished and response is ready
                 if (xhr.readyState == 4) {
                   // 200 = success status code
                     if (xhr.status == 200) {
-                      // resolve = fulfillment (1 of 2 promise states) - responseText returns the text received by the server
+                      // define what happens when promise is fulfilled - resolve = fulfilled (1 of 3 promise states) - responseText returns the text received by the server. resolve(fulfillmentValue)
                        resolve(xhr.responseText);
                     } else {
-                      // reject = rejection (2nd promise state) -
-                        reject(httpRequest().then(data => successFunction(data)).catch(error => console.log(error)));
+                      // define what happens when promise is rejected - reject = rejected ( promise state) - the string will be caught in `.catch()` method
+                        reject('request rejected');
                     }
                 }
             });
+        // opening a request triggers a change of state and prepares browser to make request- xhr.open( httpMethod, url, async)
         xhr.open ('GET','https://www.googleapis.com/books/v1/volumes?q=' + searchQuery, true);
+        // send request to server
         xhr.send();
         }
     ));
@@ -54,9 +56,9 @@ const successResult = (stringifiedData) => {
           ${linkPreview(book.volumeInfo.previewLink)}
         </div>
       `
-      //.join converts an array into a string -
+      //.join converts an array into a string
     }).join('');
-  //  adds .innerHTML so that it adds the mark up to the page once.
+  //  .innerHTML so that the the mark up is added to the page only once
     results.innerHTML = markUp;
 }
 
